6e625ec94866cfaaa021ed3ba4fdce087f502f3a
alaamhna3354 <alaamhna3354@gmail.com>
2021-06-14 21:25:42 +0300

    modi img

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -204,32 +204,19 @@
 </template>
 
 <script>
-import {
-  StatsCard,
-  ChartCard,
-  NavTabsCard,
-  NavTabsTable,
-  OrderedTable
-} from "@/components";
-
+import { StatsCard, ChartCard, NavTabsCard } from "@/components";
 export default {
-  components: {
-    StatsCard,
-    ChartCard,
-    NavTabsCard,
-    NavTabsTable,
-    OrderedTable
-  },
+  components: { StatsCard, ChartCard, NavTabsCard },
   data() {
     return {
       dailySalesChart: {
         data: {
           labels: ["M", "T", "W", "T", "F", "S", "S"],
-          series: [[12, 17, 7, 17, 23, 18, 38]]
+          series: [[12, 17, 7, 17, 23, 18, 38]],
         },
         options: {
           lineSmooth: this.$Chartist.Interpolation.cardinal({
-            tension: 0
+            tension: 0,
           }),
           low: 0,
           high: 50, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
@@ -237,19 +224,19 @@ export default {
             top: 0,
             right: 0,
             bottom: 0,
-            left: 0
-          }
-        }
+            left: 0,
+          },
+        },
       },
       dataCompletedTasksChart: {
         data: {
           labels: ["12am", "3pm", "6pm", "9pm", "12pm", "3am", "6am", "9am"],
-          series: [[230, 750, 450, 300, 280, 240, 200, 190]]
+          series: [[230, 750, 450, 300, 280, 240, 200, 190]],
         },
 
         options: {
           lineSmooth: this.$Chartist.Interpolation.cardinal({
-            tension: 0
+            tension: 0,
           }),
           low: 0,
           high: 1000, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
@@ -257,9 +244,9 @@ export default {
             top: 0,
             right: 0,
             bottom: 0,
-            left: 0
-          }
-        }
+            left: 0,
+          },
+        },
       },
       emailsSubscriptionChart: {
         data: {
@@ -275,13 +262,15 @@ export default {
             "Se",
             "Oc",
             "No",
-            "De"
+            "De",
           ],
-          series: [[542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895]]
+          series: [
+            [542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895],
+          ],
         },
         options: {
           axisX: {
-            showGrid: false
+            showGrid: false,
           },
           low: 0,
           high: 1000,
@@ -289,8 +278,8 @@ export default {
             top: 0,
             right: 5,
             bottom: 0,
-            left: 0
-          }
+            left: 0,
+          },
         },
         responsiveOptions: [
           [
@@ -300,13 +289,13 @@ export default {
               axisX: {
                 labelInterpolationFnc: function(value) {
                   return value[0];
-                }
-              }
-            }
-          ]
-        ]
-      }
+                },
+              },
+            },
+          ],
+        ],
+      },
     };
-  }
+  },
 };
 </script>
