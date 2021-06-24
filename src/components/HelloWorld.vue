864d4b45015ba3fe86ced905e61e5e962efb8f18
rahaf-jamool <rahafjamool1@gmail.com>
2021-06-24 11:10:59 +0300

    add material

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>hello</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HelloWorld",
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
